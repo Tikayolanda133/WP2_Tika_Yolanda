39268a9517395be8edfdbe6030bdacff13da0049
Tikayolanda133 <tikayoland13@gmail.com>
2022-10-18 21:27:41 +0700

    tugas pertemuan 5

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Perkenalkan";
+        echo "Nama saya Tika Yolanda, saya seorang mahasiswa kelas 12.3J.30";
+        echo "Saya dari Universitas Bina Sarana Informatika";
+        echo "Saya memiliki hobi menonton film";
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Tika Yolandda</li>
+        <li>Nama Belakang :- </li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> jl Sumber Makmur DSN II Rasau Jaya 3/li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Pontianak</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Memasak</li>
+        <li> Bernyanyi</li>
+    </ul>
+</section>
